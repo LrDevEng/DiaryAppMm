a73d88ed1580ef86dfeb174815f71e6f9f1b3490
MrLukiLegend <luki.richter@gmail.com>
2024-01-18 19:34:09 +0100

    Initial commit

---
+++ app/src/main/java/eu/merklaafe/diaryappmm/MainActivity.kt
@@ -0,0 +1,33 @@
+package eu.merklaafe.diaryappmm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import eu.merklaafe.diaryappmm.navigation.Screen
+import eu.merklaafe.diaryappmm.navigation.SetupNavGraph
+import eu.merklaafe.diaryappmm.ui.theme.DiaryAppMmTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+            DiaryAppMmTheme {
+                val navController = rememberNavController()
+                SetupNavGraph(
+                    startDestination = Screen.Authentication.route,
+                    navController = navController
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/eu/merklaafe/diaryappmm/util/Constants.kt
@@ -0,0 +1,9 @@
+package eu.merklaafe.diaryappmm.util
+
+object Constants {
+
+    const val APP_ID = "mydiaryappmm-npjwf"
+    const val CLIENT_ID = "986326035559-ht7jqc4cpokgf8hdbi2cjjhqqq52i67o.apps.googleusercontent.com"
+
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
+}
